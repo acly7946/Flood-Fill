fec0f5f6f294a7d8a51ecac6816fb4a792fc46d9
acly7946 <acly.work@tutanota.com>
2022-01-05 13:38:22 +0000

    re-implement margin and add outline

--- src/game.c
+++ src/game.c
@@ -105,29 +105,33 @@ static void floodFill(struct Grid *grid, int row, int col, int oldColor, int new
 
 static void renderGrid(struct Window *window, struct Grid *grid)
 {
+	const int margin = 9;
 	int spacing;
 	Rectangle cell;
 	Color color;
 
 	if(window->width < window->height)
 	{
-		spacing = (window->width)/grid->size;
+		spacing = (window->width-margin*2)/grid->size;
 	}
 	else
 	{
-		spacing = (window->height)/grid->size;
+		spacing = (window->height-margin*2)/grid->size;
 	}
 
 	ClearBackground(RAYWHITE);
+	// cells
 	for(int row = 0; row < grid->size; row++)
 	{
 		for(int col = 0; col < grid->size; col++)
 		{
 			color = getColor(grid->color[row][col]);
-			cell = (Rectangle){spacing*row, spacing*col, spacing, spacing};
+			cell = (Rectangle){margin+spacing*row, margin+spacing*col, spacing, spacing};
 			DrawRectangleRec(cell, color);
 		}
 	}
+	// outline
+	DrawRectangleLines(margin, margin, grid->size*spacing, grid->size*spacing, BLACK);
 }
 
 static void renderUI(struct Window *window, int turns)
